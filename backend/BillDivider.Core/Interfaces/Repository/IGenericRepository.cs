4b7e9d389d1ba4c4abb29b702445eb713d3a1dff
Nikita Labuzov <nikita.labuzov@yandex.by>
2024-12-29 20:21:37 +0300

    обновил шаблонный реп

--- backend/BillDivider.Core/Interfaces/Repository/IGenericRepository.cs
+++ backend/BillDivider.Core/Interfaces/Repository/IGenericRepository.cs
@@ -1,13 +1,65 @@
-﻿using BillDivider.Core.Entities;
+﻿using System.Linq.Expressions;
+using BillDivider.Core.Entities;
 using BillDivider.Core.Models;
 
 namespace BillDivider.Core.Interfaces.Repository;
 
 public interface IGenericRepository<T> where T : BaseEntity
 {
-    public Task CreateAsync(T entity, CancellationToken cancellationToken);
-    public Task DeleteAsync(int id, CancellationToken cancellationToken);
-    public Task<T?> GetByIdAsync(int id, CancellationToken cancellationToken);
-    public Task<List<T>> GetAllAsync(PaginationSettings paginationSettings, CancellationToken cancellationToken);
-    public Task UpdateAsync(T entity, CancellationToken cancellationToken);
+    /// <summary>
+    /// Marks entity as added in Change tracker
+    /// </summary>
+    /// <param name="entity">Entity to add</param>
+    /// <param name="cancellationToken">Task cancellation token</param>
+    /// <returns>Async operation</returns>
+    public Task CreateAsync(T entity, CancellationToken cancellationToken = default);
+    
+    /// <summary>
+    /// Marks entity as deleted in Change tracker
+    /// </summary>
+    /// <param name="entity">Entity to delete</param>
+    public void Delete(T entity);
+    
+    /// <summary>
+    /// Gets single entity or null by specified predicate
+    /// </summary>
+    /// <param name="searchPredicate">Searching predicate</param>
+    /// <param name="trackChanges">Should the entity be tracked by EF</param>
+    /// <param name="cancellationToken">Task cancellation token</param>
+    /// <param name="includeProperties">Collection of entity properties to include in request</param>
+    /// <returns>Single entity or null</returns>
+    public Task<T?> GetAsync(
+        Expression<Func<T, bool>> searchPredicate, 
+        bool trackChanges = true,
+        CancellationToken cancellationToken = default, 
+        params Expression<Func<T, object>>[] includeProperties);
+    
+    /// <summary>
+    /// Gets a collection of entities by specified predicate and pagination settings
+    /// </summary>
+    /// <param name="paginationSettings">Page size and page number</param>
+    /// <param name="searchPredicate">Searching predicate</param>
+    /// <param name="trackChanges">Should the entity be tracked by EF</param>
+    /// <param name="cancellationToken">Task cancellation token</param>
+    /// <param name="includeProperties">Collection of entity properties to include in request</param>
+    /// <returns>A collection of entities</returns>
+    public Task<T[]> GetAllAsync(
+        PaginationSettings paginationSettings,
+        Expression<Func<T, bool>>? searchPredicate,
+        bool trackChanges = true,
+        CancellationToken cancellationToken = default,
+        params Expression<Func<T, object>>[] includeProperties);
+    
+    /// <summary>
+    /// Marks entity as modified in Change tracker
+    /// </summary>
+    /// <param name="entity">Entity to update</param>
+    public void Update(T entity);
+    
+    /// <summary>
+    /// Saves pending changes to the DB
+    /// </summary>
+    /// <param name="cancellationToken">Task cancellation token</param>
+    /// <returns>Async opearation</returns>
+    public Task SaveChangesAsync(CancellationToken cancellationToken = default);
 }
--- backend/BillDivider.Infrastructure/Repositories/EFRepository/GenericRepository.cs
+++ backend/BillDivider.Infrastructure/Repositories/EFRepository/GenericRepository.cs
@@ -1,4 +1,5 @@
-﻿using BillDivider.Core.Entities;
+﻿using System.Linq.Expressions;
+using BillDivider.Core.Entities;
 using BillDivider.Core.Interfaces.Repository;
 using BillDivider.Core.Models;
 using BillDivider.Infrastructure.Context;
@@ -18,51 +19,72 @@ public class GenericRepository<T> : IGenericRepository<T> where T : BaseEntity
         _table = _context.Set<T>();
     }
 
-    public async Task CreateAsync(T entity, CancellationToken cancellationToken)
+    /// <inheritdoc/>
+    public async Task CreateAsync(T entity, CancellationToken cancellationToken = default)
     {
         await _table.AddAsync(entity, cancellationToken);
-        await _context.SaveChangesAsync(cancellationToken);
     }
 
-    public async Task DeleteAsync(int id, CancellationToken cancellationToken)
+    /// <inheritdoc/>
+    public void Delete(T entity)
     {
-        T? deletingEntity = await _table.FirstOrDefaultAsync(p => p.Id == id, cancellationToken);
-
-        if (deletingEntity == null)
-        {
-            throw new KeyNotFoundException();
-        }
-
-        _table.Remove(deletingEntity);
-        await _context.SaveChangesAsync(cancellationToken);
+        _table.Remove(entity);
     }
 
-    public async Task<T?> GetByIdAsync(int id, CancellationToken cancellationToken)
+    /// <inheritdoc/>
+    public async Task<T?> GetAsync(
+        Expression<Func<T, bool>> searchPredicate, 
+        bool trackChanges = true,
+        CancellationToken cancellationToken = default, 
+        params Expression<Func<T, object>>[] includeProperties)
     {
-        var entity = await _table.AsNoTracking().FirstOrDefaultAsync(p => p.Id == id, cancellationToken);
-
-        return entity;
+        var query = ComposeQuery(searchPredicate, trackChanges, includeProperties);
+        
+        return await query.FirstOrDefaultAsync(cancellationToken);
     }
 
-    public async Task<List<T>> GetAllAsync(PaginationSettings paginationSettings, CancellationToken cancellationToken)
+    /// <inheritdoc/>
+    public Task<T[]> GetAllAsync(
+        PaginationSettings paginationSettings,
+        Expression<Func<T, bool>>? searchPredicate,
+        bool trackChanges = true,
+        CancellationToken cancellationToken = default,
+        params Expression<Func<T, object>>[] includeProperties)
     {
-        return await _table
+        var query = ComposeQuery(searchPredicate, trackChanges, includeProperties);
+        
+        return query
             .OrderBy(p => p.Id)
             .ApplyPagination(paginationSettings)
-            .ToListAsync(cancellationToken);
+            .ToArrayAsync(cancellationToken);
     }
 
-    public async Task UpdateAsync(T entity, CancellationToken cancellationToken)
+    /// <inheritdoc/>
+    public void Update(T entity)
     {
-        var updatingEntity = await _table.AsNoTracking().FirstOrDefaultAsync(p => p.Id == entity.Id, cancellationToken);
-
-        if (updatingEntity == null)
-        {
-            throw new KeyNotFoundException();
-        }
-
         _table.Update(entity);
+    }
 
-        await _context.SaveChangesAsync(cancellationToken);
+    /// <inheritdoc/>
+    public Task SaveChangesAsync(CancellationToken cancellationToken = default)
+    {
+        return _context.SaveChangesAsync(cancellationToken);
+    }
+
+    private IQueryable<T> ComposeQuery(
+        Expression<Func<T, bool>>? searchPredicate,
+        bool trackChanges,
+        params Expression<Func<T, object>>[] includeProperties)
+    {
+        var query = trackChanges 
+            ? _table.AsQueryable().AsNoTracking() 
+            : _table.AsQueryable();
+        
+        foreach (var includeProperty in includeProperties)
+        {
+            query = query.Include(includeProperty);
+        }
+        
+        return searchPredicate is not null ? query.Where(searchPredicate) : query;
     }
 }
